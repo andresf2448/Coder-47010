352a1a8cac63bc9add6e583acdb5dccff8bbbdd4
andresf2448 <andresf2448@gmail.com>
2023-08-12 13:30:25 -0500

    Clase 5

--- Clase 5/parte2/main.js
+++ Clase 5/parte2/main.js
@@ -1,4 +1,4 @@
-let boton = document.getElementById("boton");
+// let boton = document.getElementById("boton");
 
 //primera forma de declarar un evento
 // boton.addEventListener("click", () => {
@@ -28,3 +28,75 @@ let boton = document.getElementById("boton");
 
 // boton.addEventListener("click", () => respuesta("andres"));
 //////////////////////////////////////////////
+
+let boton = document.getElementById("boton");
+
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("mouseover", () => console.log("mouseover"));
+// boton.addEventListener("mouseout", () => console.log("mouseout"));
+// boton.addEventListener("mousemove", () => console.log("mousemove"));
+// boton.addEventListener("click", () => console.log("click"));
+// boton.addEventListener("dblclick", () => console.log("dblclick"));
+
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("click", () => console.log("click"));
+
+// let saludo = document.getElementById("saludo");
+
+// boton.addEventListener("mousedown", () => {
+//   saludo.className = "rojo";
+// });
+
+// boton.addEventListener("mouseup", () => {
+//   saludo.className = "azul";
+// });
+
+// boton.addEventListener("mouseover", () => {
+//   saludo.className = "verde";
+// });
+
+// boton.addEventListener("mouseout", () => {
+//   saludo.className = "amarillo";
+// });
+
+let nombre = document.getElementById("nombre");
+let saludo = document.getElementById("saludo");
+
+// nombre.addEventListener("keydown", () => console.log("keydown"));
+// nombre.addEventListener("keyup", () => console.log("keyup"));
+
+// nombre.addEventListener("keyup", (e) => {
+//   if (e.key === "a") {
+//     saludo.className = "rojo";
+//   } else if (e.key === "s") {
+//     saludo.className = "azul";
+//   } else if (e.key === "d") {
+//     saludo.className = "verde";
+//   } else {
+//     saludo.className = "amarillo";
+//   }
+// });
+
+// nombre.addEventListener("change", () => console.log("change"));
+
+// nombre.addEventListener("input", () => {
+//   if (!nombre.value.includes("@")) {
+//     saludo.className = "rojo";
+//   } else {
+//     saludo.className = "blanco";
+//   }
+// });
+
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputs = e.target.children;
+
+  if (!inputs[0].value.includes("@")) {
+    inputs[0].value = "";
+  }
+});
