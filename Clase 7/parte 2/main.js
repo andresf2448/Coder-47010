6234f24c531192de95f464c1b821530e666925c8
andresf2448 <andresf2448@gmail.com>
2023-08-26 13:23:56 -0500

    Clase 7

--- Clase 7/parte 2/main.js
+++ Clase 7/parte 2/main.js
@@ -81,3 +81,86 @@ let boton = document.getElementById("boton");
 //     }
 //   });
 // });
+
+//TOASTIFY
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//     style: {
+//       background: "linear-gradient(to right, #00b09b, #96c92d)",
+//     },
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Probando toast!",
+//     duration: 3000,
+//     onClick: () => {
+//       Toastify({
+//         text: "Clickeaste un Toast!",
+//         duration: 1500,
+//       }).showToast();
+//     },
+//   }).showToast();
+// });
+
+// boton.addEventListener("click", () => {
+//   Toastify({
+//     text: "Click aquí para ir a Coder!",
+//     duration: 3000,
+//     destination: "https://www.coderhouse.com",
+//   }).showToast();
+// });
+
+//LUXON
+// const DateTime = luxon.DateTime;
+
+// const hoy = DateTime.local(2023, 8, 26);
+// console.log(hoy.c);
+
+// const now = DateTime.now();
+
+// console.log(now.toString());
+// console.log(now.year);
+// console.log(now.month);
+// console.log(now.day);
+// console.log(now.second);
+// console.log(now.weekday);
+// console.log(now.zoneName);
+// console.log(now.daysInMonth);
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleString(DateTime.DATE_FULL));
+// console.log(now.toLocaleString(DateTime.TIME_SIMPLE));
+// console.log(now.setLocale('en').toLocaleString(DateTime.DATE_FULL));
+// console.log(now.setLocale('es').toLocaleString(DateTime.DATE_FULL));
+
+// const now = DateTime.now();
+
+// console.log(now.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const suma = now.plus({ hours: 1, minutes: 10 });
+// console.log(suma.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const resta = now.minus({ month: 1, days: 1 });
+// console.log(resta.toLocaleString(DateTime.DATETIME_SHORT));
+
+const DateTime = luxon.DateTime;
+const Interval = luxon.Interval;
+
+const now = DateTime.now();
+const navidad = DateTime.local(2023, 12, 25);
+
+const i = Interval.fromDateTimes(now, navidad);
+
+console.log(i.length("days"));
+console.log(i.length("hours"));
+console.log(i.length("minutes"));
